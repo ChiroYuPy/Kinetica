7eeb3c0a879bede11b317d8aa15480e68b8f1b5d
adrian <adrileroux9@gmail.com>
2026-04-13 22:13:49 +0200

    change the naive broad phase for a sweep and prune algorithm, and simplify the module : trait removal

--- kinetica/src/math/mod.rs
+++ kinetica/src/math/mod.rs
@@ -2,8 +2,10 @@ pub mod types;
 pub mod utils;
 mod constants;
 mod geometry;
+mod aabb;
 
 pub use types::*;
 pub use constants::*;
 pub use utils::*;
 pub use geometry::*;
+pub use aabb::{AABB, intersects, merge};
